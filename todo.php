987e6a6477d959d1991ec977b2e8297379a3aad1
Reverai <shedow99@bk.ru>
2019-06-08 23:57:55 +0300

    list html

---
+++ todo.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <script src="ajax.js"></script>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<title>TODOO</title>
+</head>
+<body>
+	<style > 
+body 
+{ 
+background: url(https://i.pinimg.com/originals/b6/45/21/b645213afd23b2954a00e11afe35515d.jpg) no-repeat; 
+background-size: 100%; 
+} 
+</style> 
+		<div align="center" id="l1">
+		<h4>
+		<p>
+			<label >TODOOO</label><br>
+			<div align="center">
+		</div>
+		</p>
+		<p>
+			<h4>
+			<label>Your List</label><br>
+			<div>
+			<input type="text" name="addMessage" class="form-control" id="l2">
+			<input  id="SHOW" value="add" id="button" class="btn btn-outline-info" type="submit" onclick="add()">
+			<ul id="task">
+				
+			</ul>
+			</div>
+		</p>
+	</div>
+	</h4>
+		<style type="text/css">
+   #l2 {
+   	width: 300px;
+   }
+
+</body>
+
+</html>
